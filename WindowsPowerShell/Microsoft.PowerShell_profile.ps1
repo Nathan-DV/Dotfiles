011fdf2d80dd7ea77f507b13ba4ea35ca17c59d4
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-04-21 08:55:57 -0400

    Update Microsoft.PowerShell_profile.ps1

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,2 @@
 clear
 Invoke-Expression (&starship init powershell)
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
